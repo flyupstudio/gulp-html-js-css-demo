e00a50316953a5829920706784b3649b46c4296a
Maxim Vlasenko <vlasenko.maxim@gmail.com>
2016-06-21 16:03:05 +0300

    #sprite and remove minify (no ned on development)

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,10 @@ var del = require('del');
 var connect = require('gulp-connect');
 var cleanCSS = require('gulp-clean-css');
 
+var sprite = require('gulp-sprite-generator');
+
+
+
 gulp.task('connect', function() {
     return connect.server({
         livereload: true
@@ -19,22 +23,17 @@ gulp.task('connect', function() {
 
 gulp.task('minify-css', function() {
     return gulp.src('./development/styles/*.css')
-        .pipe(cleanCSS({compatibility: 'ie8'}))
+       // .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(gulp.dest('./dist/styles/'));
 });
 
 gulp.task('minify-img', function() {
     return gulp.src('./development/images/**/*')
-        .pipe(imagemin())
+       // .pipe(imagemin())
         .pipe(gulp.dest('./dist/images'))
 });
 
 
-gulp.task('minify-img1', function() {
-    return gulp.src('./development/styles/images/*')
-        .pipe(imagemin())
-        .pipe(gulp.dest('./dist/styles/images'))
-});
 
 gulp.task('copy-html', function() {
     gulp.src('./development/*.html')
@@ -43,19 +42,35 @@ gulp.task('copy-html', function() {
 
 gulp.task('js', function () {
     gulp.src('development/scripts/*.js')
-        .pipe(jsmin())
+       // .pipe(jsmin())
         .pipe(browserify({
             insertGlobals : true
         }))
         .pipe(gulp.dest('dist/scripts/'));
 });
 
+gulp.task('sprites', function(){
+
+    var spriteOutput;
+
+    spriteOutput = gulp.src("./dist/styles/*.css")
+        .pipe(sprite({
+            baseUrl:         "./development/images/",
+            spriteSheetName: "sprite.png",
+            spriteSheetPath: "/dist/images/sprites/"
+        }));
+
+    spriteOutput.css.pipe(gulp.dest("./dist/styles"));
+    spriteOutput.img.pipe(gulp.dest("./dist/image/"));
+
+});
+
 gulp.task('watch', function(){
     gulp.watch(['./development/scripts/*.js'], ['js']);
     gulp.watch(['./development/styles/*.css'], ['minify-css']);
     gulp.watch(['./development/images/*.*'], ['minify-img']);
-    gulp.watch(['./development/styles/images/*.*'], ['minify-img1']);
+    gulp.watch(['./development/images/*.*'], ['sprites']);
     gulp.watch(['./development/*.html'], ['copy-html']);
 });
 
-gulp.task('default', ['connect', 'watch', 'js', 'minify-css','minify-img','minify-img1', 'copy-html']);
+gulp.task('default', ['connect', 'watch', 'js','sprites', 'minify-css','minify-img','copy-html']);
